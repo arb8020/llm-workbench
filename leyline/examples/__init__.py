0e8f14d4f90038904c930b6197aec8ee24c01766
arb8020 <arbeightytwenty@gmail.com>
2025-08-29 22:52:35 -0700

    Remove leyline module

--- leyline/examples/__init__.py
+++
@@ -1,1 +0,0 @@
-# Example workers for leyline framework
--- leyline/leyline/__init__.py
+++
@@ -1,6 +0,0 @@
-from .worker_base import WorkerBase, expose
-from .controller import Controller
-from .common import HEARTBEAT, generate_rid, build_request_frame, build_chunk_frame, parse_frame
-
-__version__ = "0.1.0"
-__all__ = ["WorkerBase", "expose", "Controller", "HEARTBEAT", "generate_rid", "build_request_frame", "build_chunk_frame", "parse_frame"]
